0b8cb9d85ac6412f8efe70f27734fbaef968f446
bitbloxhub <45184892+bitbloxhub@users.noreply.github.com>
2022-06-29 12:44:47 -0700

    hello git

---
+++ src/main/java/fabric_mods/sculk_shrieker_summons/SculkShreikerSummons.java
@@ -0,0 +1,14 @@
+package fabric_mods.sculk_shrieker_summons;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SculkShreikerSummons implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("sculk_shrieker_summons");
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/fabric_mods/sculk_shrieker_summons/mixin/SculkShriekerPlace.java
@@ -0,0 +1,30 @@
+package fabric_mods.sculk_shrieker_summons.mixin;
+
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.BlockEntityType;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+
+@Mixin(BlockItem.class)
+public class SculkShriekerPlace {
+	@Inject(at = @At("HEAD"), method = "Lnet/minecraft/item/BlockItem;postPlacement(Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;Lnet/minecraft/block/BlockState;)Z	")
+	private void postPlacement(BlockPos pos, World world, @Nullable PlayerEntity player, ItemStack stack,
+			BlockState state, CallbackInfoReturnable<Boolean> cir) {
+		BlockEntity blockEntity = world.getBlockEntity(pos);
+		if (blockEntity.getType() == BlockEntityType.SCULK_SHRIEKER) {
+			world.setBlockState(pos,
+					state.with(BooleanProperty.of("can_summon"), true));
+		}
+	}
+}
